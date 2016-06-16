13d4a1be4851742f4d7f7c5fda49cfe70cfe169e
ohmystack <jiangjun1990@gmail.com>
2016-06-17 01:31:08 +0800

    Refine env.sh network settings
    Use auto-detected IP Address for HOST_IP.
    Add PUBLIC_NETWORK for public IP range.

--- env.sh
+++ env.sh
@@ -13,8 +13,10 @@ export HOST_ID=$(hostname)
 export LOCAL_NIC="eth0"
 export LOCAL_NIC_IP=$(ip addr show $LOCAL_NIC | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
 # HOST_IP is the VM's public IP, for remote communication
-export HOST_IP='127.0.0.1'
+export HOST_IP=$LOCAL_NIC_IP
 export LIBPROCESS_IP=$LOCAL_NIC_IP
+# PUBLIC_NETWORK is the VM's public network IP ranges, a CIDR address. e.g. 10.0.2.0/24
+export PUBLIC_NETWORK=$(ip r | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/[0-9]\{1,2\}' | head -1)
 
 # -----------------
 # Zookeeper
