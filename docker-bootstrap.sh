120a608e7cc8c84a8944fcfe32566e630a1d1792
inoc603 <inoc603@gmail.com>
2017-02-27 18:30:01 +0800

    ignore config file when starting docker-bootstrap

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -35,6 +35,7 @@ start_daemon() {
 		dockerd \
 			-H ${BOOTSTRAP_SOCK} \
 			-p ${BOOTSTRAP_PID} \
+                        --config-file "" \
 			--iptables=false \
 			--ip-masq=false \
 			--bridge=none \
